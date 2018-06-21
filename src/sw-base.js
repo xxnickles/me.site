339ef6902a9c5f172b73d55bb0cdf13c7ceab598
nickle <xxnickles@gmail.com>
2018-06-20 21:52:52 -0400

    Adding initial circleci config

--- src/sw-base.js
+++ src/sw-base.js
@@ -1,4 +1,4 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox-sw.js");
 workbox.skipWaiting();
 workbox.clientsClaim();
 
