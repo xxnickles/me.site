6ba9f5f89e8f771ae564c9865be41912f62079c9
nickle <xxnickles@gmail.com>
2018-10-07 21:38:38 -0400

    Refactoring layout and color scheme

--- public/sw.js
+++ public/sw.js
@@ -26,4 +26,21 @@ workbox.routing.registerRoute(
     workbox.strategies.networkFirst()
 );
 
-workbox.precaching.precacheAndRoute([]);
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "app.21f0d3cb.js",
+    "revision": "cf25e5c527a7d25b3ac3f97fe1a9179f"
+  },
+  {
+    "url": "app.3520ee44.css",
+    "revision": "b15068fcdc6d1cc10121ded38f1f304a"
+  },
+  {
+    "url": "app.3520ee44.js",
+    "revision": "2ac6352cc83e30774444ca2e85bc5148"
+  },
+  {
+    "url": "app.724837c9.css",
+    "revision": "5b35ad99869bc460fe50452669b94055"
+  }
+]);
