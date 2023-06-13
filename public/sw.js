a41565bfbc425a1f4c02b22e331678638ea545a4
Alex Fonseca <AFonseca@aamva.org>
2023-06-13 10:09:36 -0400

    Updating dependencies and information

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.1/workbox-sw.js');
-precacheAndRoute([])
+precacheAndRoute([{"revision":"13fdf671757a9f13c7f357c4f6bff3e0","url":"index.301580f7.js"},{"revision":"45d7bb352f701f7d0011be5951cc4eb5","url":"index.867122b9.css"},{"revision":"44e989814c932a2102b033976054bf81","url":"sw.js"}])
 
 workbox.skipWaiting();
 workbox.clientsClaim();
--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,11 +1,9 @@
 import '../styles/styles.scss';
-import { initBarAnimation } from './skills-animation';
 import { addOfflineChecking } from './connection-checker';
 import { setFooterDate } from './footerDate';
 
-initBarAnimation();
 setFooterDate('footer > section#year')
 const offlineElement = document.getElementById('connection-status');
-if(offlineElement) addOfflineChecking(offlineElement);
+if (offlineElement) addOfflineChecking(offlineElement);
 
 
